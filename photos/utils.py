da8e14092cfe061a6ea34cd9dc84e7048ea6f1c3
alexHooper <alexmohack@gmail.com>
2019-01-02 09:13:15 +0530

    change the comment

--- photos/utils.py
+++ photos/utils.py
@@ -1,3 +1,3 @@
 def photo_directory_path(instance, filename):
-	"""file will be saved to MEDIA_ROOT/photos/2015/01/30"""
+	"""file will be saved to MEDIA_ROOT/photos/2018/01/2"""
 	return 'photos/%Y/%m/%d/'

05d7f0b2d3c964aff6f12e21a3dfca00ddd068db
alexHooper <alexmohack@gmail.com>
2019-01-02 11:09:09 +0530

    correct the path for media uploads

--- photos/utils.py
+++ photos/utils.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+now = datetime.now()
+
 def photo_directory_path(instance, filename):
 	"""file will be saved to MEDIA_ROOT/photos/2018/01/2"""
-	return 'photos/%Y/%m/%d/'
+	return f'photos/{now.year}/{now.month}/{now.date().day}/{filename}'

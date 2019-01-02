9744bf7a671e84caaec5ad211c082919a9ee8492
alexHooper <alexmohack@gmail.com>
2019-01-02 09:00:38 +0530

    create model for photos app

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from .utils import photo_directory_path
+
+class Photo(models.Model):
+	title = models.CharField(max_length=256)
+	file = models.ImageField(upload_to=photo_directory_path)
+	timestamp = models.DateTimeField(auto_now=True)
+	updated = models.DateTimeField(auto_now_add=True)

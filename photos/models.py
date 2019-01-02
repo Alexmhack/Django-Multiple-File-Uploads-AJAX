6a8a5f6ae8d28ca6449d22bb9b23d5b7e7fd55fb
alexHooper <alexmohack@gmail.com>
2019-01-02 10:52:33 +0530

    make title field blank

--- photos/models.py
+++ photos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from .utils import photo_directory_path
 
 class Photo(models.Model):
-	title = models.CharField(max_length=256)
+	title = models.CharField(max_length=256, blank=True, null=True)
 	file = models.ImageField(upload_to=photo_directory_path)
 	timestamp = models.DateTimeField(auto_now=True)
 	updated = models.DateTimeField(auto_now_add=True)

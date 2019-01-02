ce07dd82597aaa27551ccf621e8d2740a35152dd
alexHooper <alexmohack@gmail.com>
2019-01-02 09:26:04 +0530

    add url for photos app

--- multiple/urls.py
+++ multiple/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('photos/', include('photos.urls', namespace='photos'))
 ]
 
 if settings.DEBUG:

da8ddfca86525f15ab3d6603bd4ccfa7206aae38
alexHooper <alexmohack@gmail.com>
2019-01-02 19:00:59 +0530

    add progress bar js code

--- static/jquery/js/basic-upload.js
+++ static/jquery/js/basic-upload.js
@@ -5,16 +5,48 @@ $(function() {
 		$("#fileupload").click();
 	});
 
+	//////////////////////////////
+	// upload without progress bar
+	//////////////////////////////
+	
+	// $("#fileupload").fileupload({
+	// 	dataType: "json",
+	// 	done: (e, data) => {
+	// 		console.log(data);
+	// 		if (data.result.is_valid) {
+	// 			$("#gallery tbody").append(
+	// 				"<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
+	// 			)
+	// 		}
+	// 	}
+	// })
+		
+	//////////////////////////////
+	// upload with progress bar
+	//////////////////////////////
+	
 	$("#fileupload").fileupload({
-		dataType: "json",
+		dataType: 'json',
+		sequentialUploads: true, // upload files one by one
+		start: (e) => {	// when the uploading starts show the modal
+			$("#modal-progress").modal("show");
+		},
+		stop: (e) => {	// when the uploading completes hide the modal
+			$("#modal-progress").modal("hide");
+		},
+		progressall: (e, data) => { // update the progress bar
+			var progress = parseInt(data.loaded / data.total * 100, 10);
+			var strProgress = progress + '%';
+			$(".progress-bar").css({"width": strProgress});
+			$(".progress-bar").text(strProgress);
+		},
 		done: (e, data) => {
-			console.log(data);
 			if (data.result.is_valid) {
-				$("#gallery tbody").append(
+				$("#gallery tbody").prepend(
 					"<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
 				)
 			}
 		}
 	})
-		
+
 })

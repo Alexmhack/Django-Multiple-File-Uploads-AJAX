fc007a865b3436ed57c34502e33ad50be831891a
alexHooper <alexmohack@gmail.com>
2019-01-02 09:26:18 +0530

    create view for basic file uploads

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Photo
+from .forms import PhotoForm
+
+class BasicFileUploadView(View):
+	def get(self, request):
+		photos = Photo.objects.all()
+		context = {'photos': photos}
+		return render(request, 'photos/basic_upload.html', context)
+
+	def post(self, request):
+		form = PhotoForm(self.request.POST, self.request.FILES)
+		if form.is_valid():
+			photo = form.save()
+			data = {'is_valid': True, 'name': photo.file.name, 'url': photo.file.url}
+		else:
+			data = {'is_valid': False}
+		return JsonResponse(data)

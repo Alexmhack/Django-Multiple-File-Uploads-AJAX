83ca3fef9eb5862ff015441e72b577140648be59
alexHooper <alexmohack@gmail.com>
2019-01-02 08:36:42 +0530

    configure media settings

--- multiple/settings.py
+++ multiple/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
